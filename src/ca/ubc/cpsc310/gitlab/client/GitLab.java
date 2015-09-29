6695bd6dd52335a0d5fda014d58d46ee94470517
jasonmasih <j.p.masih@gmail.com>
2015-09-28 19:47:23 -0700

    Added comment

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -84,6 +84,7 @@ public class GitLab implements EntryPoint {
 		flexTable.setText(0,2, "Shopping Cart Size");
 		flexTable.setText(0,3, "Wish List Size");
 		flexTable.setStyleName("centered-table", true);
+		// JASON'S COMMENT
 		
 		for(int i=0; i < users.size(); i++)
 		{
